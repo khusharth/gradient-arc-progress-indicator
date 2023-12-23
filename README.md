a7b1e3b9508a24fa8901b9143e1c1179c4fa72b1
khusharth <khusharth19@gmail.com>
2023-12-23 17:51:25 +0530

    chore: setup typescript

---
+++ README.md
@@ -0,0 +1,1 @@
+## Gradient Arc Progress Indicator
---
+++ src/GradientArcProgressIndicator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const GradientArcProgressIndicator = () => {
+  return (
+    <View>
+      <Text>Gradient Arc Progress Indicator</Text>
+    </View>
+  );
+};
+
+export default GradientArcProgressIndicator;
