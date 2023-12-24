ce5b6b39ad88983bb9dec5731739c97bf92c433c
khusharth <khusharth19@gmail.com>
2023-12-24 18:59:35 +0530

    chore: setup linting

--- src/GradientArcProgressIndicator.tsx
+++ src/GradientArcProgressIndicator.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Abc from './abc';
 
 const GradientArcProgressIndicator = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Gradient Arc Progress Indicator</Text>
+      <Abc />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default GradientArcProgressIndicator;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./GradientArcProgressIndicator";
+export { default } from './GradientArcProgressIndicator';
